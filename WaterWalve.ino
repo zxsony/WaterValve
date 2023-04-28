38e008ce551d975ee5a236c346005e73aa4cfa8d
zxsony <zxsony@list.ru>
2023-04-28 13:53:11 +0300

    add wildcard

---
+++ WaterWalve.ino
@@ -0,0 +1,93 @@
+#include <esp-fs-webserver.h>  // https://github.com/cotestatnt/esp-fs-webserver
+
+#include <FS.h>
+#include <LittleFS.h>
+#define FILESYSTEM LittleFS
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 1
+#endif
+
+// In order to set SSID and password open the /setup webserver page
+// const char* ssid;
+// const char* password;
+
+uint8_t ledPin = LED_BUILTIN;
+bool apMode = false;
+
+#ifdef ESP8266
+  ESP8266WebServer server(80);
+#elif defined(ESP32)
+  WebServer server(80);
+#endif
+FSWebServer myWebServer(FILESYSTEM, server);
+
+
+////////////////////////////////  Filesystem  /////////////////////////////////////////
+void startFilesystem(){
+  // FILESYSTEM INIT
+  if ( FILESYSTEM.begin()){
+    File root = FILESYSTEM.open("/", "r");
+    File file = root.openNextFile();
+    while (file){
+      const char* fileName = file.name();
+      size_t fileSize = file.size();
+      Serial.printf("FS File: %s, size: %lu\n", fileName, (long unsigned)fileSize);
+      file = root.openNextFile();
+    }
+    Serial.println();
+  }
+  else {
+    Serial.println("ERROR on mounting filesystem. It will be formmatted!");
+    FILESYSTEM.format();
+    ESP.restart();
+  }
+}
+
+////////////////////////////  HTTP Request Handlers  ////////////////////////////////////
+void handleLed() {
+  WebServerClass* webRequest = myWebServer.getRequest();
+
+  // http://xxx.xxx.xxx.xxx/led?val=1
+  if(webRequest->hasArg("val")) {
+    int value = webRequest->arg("val").toInt();
+    digitalWrite(ledPin, value);
+    //delay (2000);
+    //Serial.print(value);
+  }
+
+  String reply = "LED is now ";
+  reply += digitalRead(ledPin) ? "OFF" : "ON";
+  webRequest->send(200, "text/plain", reply);
+}
+
+
+void setup(){
+  Serial.begin(115200);
+
+  // FILESYSTEM INIT
+  startFilesystem();
+
+  // Try to connect to flash stored SSID, start AP if fails after timeout
+  IPAddress myIP = myWebServer.startWiFi(15000, "ESP8266_AP", "123456789" );
+
+  // Add custom page handlers to webserver
+  myWebServer.addHandler("/led", HTTP_GET, handleLed);
+
+  // Start webserver
+  if (myWebServer.begin()) {
+    Serial.print(F("ESP Web Server started on IP Address: "));
+    Serial.println(myIP);
+    Serial.println(F("Open /setup page to configure optional parameters"));
+    Serial.println(F("Open /edit page to view and edit files"));
+    Serial.println(F("Open /update page to upload firmware and filesystem updates"));
+  }
+
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+
+void loop() {
+  myWebServer.run();
+
+}
